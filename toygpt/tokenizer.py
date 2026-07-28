00a673824461f92844a4ce936603cde3a4fb479e
Rasmus <rsvedberg2001@gmail.com>
2026-07-28 12:53:18 +0200

    Train

--- toygpt/tokenizer.py
+++ toygpt/tokenizer.py
@@ -95,30 +95,30 @@ class bpe:
         return ids_list
 
 
-#class tokenizer(object):
-#    def __init__ (self, text):
-#        preprocessed = re.sub(r"[^a-zA-Z0-9\s]", "", text.lower())
-#        preprocessed = preprocessed.split()
-#
-#        word_list = sorted(set(preprocessed))
-#        word_list.extend(["<|unk|>", "<|END|>"])
-#
-#        self.str_to_int = {word:id for id, word in enumerate(word_list)}
-#        self.int_to_str = {id:word for word, id in self.str_to_int.items()}
-#        
-#
-#    def encoder(self, text):
-#        # Convert raw text into a list of integer IDs
-#        preprocessed = re.sub(r"[^a-zA-Z0-9\s]", "", text.lower())
-#        preprocessed = preprocessed.split()
-#
-#        unk_id = self.str_to_int["<|unk|>"]
-#        mapping = [self.str_to_int.get(word, unk_id) for word in preprocessed]
-#        return mapping
-#
-#    def decoder(self, ids):
-#        text = " ".join([self.int_to_str[i] for i in ids])
-#        return text 
+class tokenizer(object):
+    def __init__ (self, text):
+        preprocessed = re.sub(r"[^a-zA-Z0-9\s]", "", text.lower())
+        preprocessed = preprocessed.split()
+
+        word_list = sorted(set(preprocessed))
+        word_list.extend(["<|unk|>", "<|END|>"])
+
+        self.str_to_int = {word:id for id, word in enumerate(word_list)}
+        self.int_to_str = {id:word for word, id in self.str_to_int.items()}
+        
+
+    def encoder(self, text):
+        # Convert raw text into a list of integer IDs
+        preprocessed = re.sub(r"[^a-zA-Z0-9\s]", "", text.lower())
+        preprocessed = preprocessed.split()
+
+        unk_id = self.str_to_int["<|unk|>"]
+        mapping = [self.str_to_int.get(word, unk_id) for word in preprocessed]
+        return mapping
+
+    def decoder(self, ids):
+        text = " ".join([self.int_to_str[i] for i in ids])
+        return text 
     
 
 with open ("data/the-verdict.txt", "r") as f:
--- toygpt/train.py
+++ toygpt/train.py
@@ -2,11 +2,57 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+# Trainging step: Clear gradiant from prev -> run model to get logits -> find loss from those digits compared to target ->
+# call backward to compute gradiant -> call optimizer step to actaully update weigths
 def train_step(model, input_batch, target_batch, optimizer):
     optimizer.zero_grad()
     output = model(input_batch)
-    loss = F.cross_entropy(output, target_batch)
+    vocab_size = output.shape[-1]
+    loss = F.cross_entropy(output.view(-1, vocab_size), target_batch.view(-1))
     loss.backward()
     optimizer.step()
     return loss.item()
 
+
+def train_model(model, dataloader, optimizer, num_epochs):
+    model.train()
+    for epoch in range(num_epochs):
+        
+        for tuple in dataloader:
+            input_ids = tuple[0]
+            target_ids = tuple[1]
+            batch = torch.cat(input_ids, target_ids)
+            print(batch)
+        #input_batch = torch.tensor(input_ids)
+        #target_batch = torch.tensor(target_ids)
+        train_step(model, input_ids, target_ids, optimizer)
+    pass
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+from dataset import GPTDataset
+
+# small deterministic corpus, vocab_size=8
+token_ids = [1,2,3,4,5,6,7,1,2,3,4,5,6,7,1,2]
+ds = GPTDataset(token_ids, context_size=4, stride=2)
+print(ds[0])
+dl = DataLoader(ds, batch_size=2, shuffle=False)
+
+class TinyModel(nn.Module):
+    def __init__(self, vocab_size, d_model):
+        super().__init__()
+        self.emb = nn.Embedding(vocab_size, d_model)
+        self.out = nn.Linear(d_model, vocab_size)
+    def forward(self, ids):
+        return self.out(self.emb(ids))
+
+torch.manual_seed(42)
+model = TinyModel(vocab_size=8, d_model=8)
+optimizer = torch.optim.AdamW(model.parameters(), lr=0.01)
+
+losses = train_model(model, dl, optimizer, num_epochs=2)
+print(len(losses))   # 6  (6 windows / batch_size=2 = 3 batches per epoch, x 2 epochs)
+for l in losses:
+    print(l)
