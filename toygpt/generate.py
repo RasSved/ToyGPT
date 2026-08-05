7aef46c1859e6d0bb434304087941aec1c009dfe
Rasmus <rsvedberg2001@gmail.com>
2026-08-05 12:14:04 +0200

    Updated Generate to use the new predictions

--- toygpt/generate.py
+++ toygpt/generate.py
@@ -1,4 +1,4 @@
-from model import gpt_model
+from model import gpt_model, GPTModel
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -42,14 +42,41 @@ def sample_next_token(logits, temperature=1.0, top_k=None):
 # KV-cache for real models TODO
 # Call gpt model to get logits then with our greedy pick we get the prediction for "next word"
 # This is repeated for however many new token we want
-def generate(ids, num_new_tokens, token_embedding_matrix, pos_embedding_matrix, block_params_list, gamma_f, beta_f, W_out, b_out):
-    for t in range(num_new_tokens):
+def generatev1(ids, num_new_tokens, token_embedding_matrix, pos_embedding_matrix, block_params_list, gamma_f, beta_f, W_out, b_out):
+    for _ in range(num_new_tokens):
         gpt = gpt_model(ids, token_embedding_matrix, pos_embedding_matrix, block_params_list, gamma_f, beta_f, W_out, b_out)
         prediction = next_token_id(gpt[-1])
         ids = ids.copy()
         ids.append(prediction)
     return ids
 
-def generatev2(models, ids, num_new_tokens, conext_length, temperature=1.0, top_k=None):
-    pass
 
+# Cropp the input ids if they are outside tokenlen range -> get logits from our model -> 
+# look at the last logits and get a next token prediction from it -> add the prediction at the end 
+# of our ids. (Basically adding a word at the end of a sentence using our prediction)
+def generate(model, ids, num_new_tokens, context_length, temperature=1.0, top_k=None): 
+    for _ in range(num_new_tokens):
+        cropped = ids if ids.shape[1] <= context_length else ids[:, -context_length:]
+        logits = model(cropped)
+        gpt = logits[0, -1, :]
+        next_t = sample_next_token(gpt, temperature, top_k)
+        ids = torch.cat([ids, torch.tensor([[next_t]])], dim=1)
+    return ids 
+
+torch.manual_seed(123)
+
+model = GPTModel(vocab_size=10, d_model=8, num_heads=2, num_layers=2, d_ff=16, context_length=6)
+ids = torch.tensor([[1, 2, 3]])   # shape (1, 3) -- note the batch dimension
+
+torch.manual_seed(0)
+out1 = generate(model, ids, num_new_tokens=4, context_length=6, temperature=1.0, top_k=None)
+print(out1)   # tensor([[1, 2, 3, 6, 1, 2, 3]])
+
+torch.manual_seed(0)
+out2 = generate(model, ids, num_new_tokens=4, context_length=6, temperature=1.0, top_k=3)
+print(out2)   # tensor([[1, 2, 3, 4, 1, 0, 2]])
+
+torch.manual_seed(0)
+out3 = generate(model, ids, num_new_tokens=8, context_length=6, temperature=1.0, top_k=None)
+print(out3)          # tensor([[1, 2, 3, 6, 1, 2, 3, 1, 8, 1, 4]])
+print(out3.shape[1]) # 11
